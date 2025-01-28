7c74d655ddd1406566c765cec1dfc536d720c908
Melanty <107343612+Me1anty@users.noreply.github.com>
2025-01-29 01:52:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (req, res) => {
   // Перенаправление всех запросов на api.zelenka.guru
   const proxy = createProxyMiddleware({
-    target: 'https://api.zelenka.guru', // Целевой домен
+    target: 'https://api.lzt.market', // Целевой домен
     changeOrigin: true, // Изменяет origin запроса на целевой домен
     onProxyReq: (proxyReq, req) => {
       // Обновляем заголовки, если нужно
-      proxyReq.setHeader('host', 'api.zelenka.guru');
+      proxyReq.setHeader('host', 'api.lzt.market');
     },
   });
 
